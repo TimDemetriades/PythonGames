b70d60afda61b6b74dc14187d6faeadd4010a23e
Tim Demetriades <tim.demetriades@gmail.com>
2020-01-07 17:25:41 -0500

    Part 1 - Getting Started

--- Pong.py
+++ Pong.py
@@ -1,1 +1,14 @@
 # Pong
+
+import turtle   #module for simple games and graphics
+
+wn = turtle.Screen()    #captial S is important
+wn.title("Pong by Tim Demetriades")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)    #stops window from updating, speeds up the game
+
+#Main game loop
+while True;
+    wn.update() #everytime the loop runs it updates the screen
+    
