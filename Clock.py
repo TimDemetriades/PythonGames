70c7c7d9ba38251e79a9c10acec4194ec537aa00
Tim Demetriades <tim.demetriades@gmail.com>
2020-01-08 14:32:07 -0500

    Simple Analog Clock

---
+++ Clock.py
@@ -0,0 +1,19 @@
+# Simple Analog Clock
+
+import turtle           # turtle module
+wn = turtle.Screen()    # sets up screen window
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.title("Simple Analog Clock by Tim Demetriades")
+
+
+
+
+
+
+
+
+
+
+
+wn.mainloop()   #keeps window from closing
