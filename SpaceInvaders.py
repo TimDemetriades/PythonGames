967039048e61c3e65f15f65159f00cf781aea910
Tim Demetriades <tim.demetriades@gmail.com>
2020-01-17 19:58:56 -0500

    Space Invaders - Added title, logo, and background color

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -8,6 +8,11 @@ pygame.init()
 # Create the screen , hieght = 800 and width = 600
 screen = pygame.display.set_mode((800,600)) 
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("spaceship.png")   
+pygame.display.set_icon(icon)
+
 # Game Loop
 # Prevents screen from closing until you quit
 running = True  # make false to close screen
@@ -15,3 +20,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # RGB - Red, Green, Blue 
+    screen.fill((0, 0, 0))      # 0,0,0 = black
+    pygame.display.update()     # update screen
