1988b55d793783bbe389d35a14f2d9ac6c5ab705
Tim Demetriades <tim.demetriades@gmail.com>
2020-01-17 19:46:20 -0500

    Space Invaders - Created game window

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,1 +1,17 @@
 # Space Invaders
+
+import pygame
+
+# Initialize the pygame
+pygame.init()
+
+# Create the screen , hieght = 800 and width = 600
+screen = pygame.display.set_mode((800,600)) 
+
+# Game Loop
+# Prevents screen from closing until you quit
+running = True  # make false to close screen
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
