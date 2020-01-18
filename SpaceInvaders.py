044e6e00ba4a560f3005bdc64e074f00c0aaa6c3
Tim Demetriades <tim.demetriades@gmail.com>
2020-01-18 17:02:04 -0500

    Space Invaders - Added background image

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -9,6 +9,9 @@ pygame.init()
 # Create the screen , width = 800 and height = 600
 screen = pygame.display.set_mode((800,600)) 
 
+# Background
+background = pygame.image.load("background1.png")
+
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("spaceship.png")   
@@ -24,7 +27,7 @@ playerX_change = 0.0
 enemyImg = pygame.image.load("mike.png")
 enemyX = random.randint(0,800)      # spawns between 0 and 800 on x-axis
 enemyY = random.randint(50,150)     # spawns between 50 and 150 on y-axis
-enemyX_change = 0.15
+enemyX_change = 2
 enemyY_change = 40
 
 def player(x,y):
@@ -40,7 +43,9 @@ running = True  # make false to close screen
 while running:
 
     # RGB - Red, Green, Blue 
-    screen.fill((255, 0, 0))      # 0,0,0 = black
+    screen.fill((0, 0, 0))      # 0,0,0 = black
+    # Background Image
+    screen.blit(background,(0,0))
 
     for event in pygame.event.get():    #checks if event occured
         if event.type == pygame.QUIT:
@@ -49,9 +54,9 @@ while running:
     # if key is pressed, check if it was right or left
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
-            playerX_change = -0.15
+            playerX_change = -3
         if event.key == pygame.K_RIGHT:
-            playerX_change = 0.15
+            playerX_change = 3
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             playerX_change = 0.0
@@ -68,10 +73,10 @@ while running:
     # Enemy movement
     enemyX = enemyX + enemyX_change
     if enemyX <= 0:
-        enemyX_change = 0.15
+        enemyX_change = 2
         enemyY = enemyY + enemyY_change
     elif enemyX >= 800 - 64:   # screen width - player width
-        enemyX_change = -0.15
+        enemyX_change = -2
         enemyY = enemyY + enemyY_change
 
     player(playerX,playerY)    #must draw player after drawing screen so it appears on top
