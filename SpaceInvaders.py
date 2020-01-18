fe4afbf0f658ba04ec1c138e1969851561492ffa
Tim Demetriades <tim.demetriades@gmail.com>
2020-01-17 20:10:01 -0500

    Space Invaders - Added player image

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -5,7 +5,7 @@ import pygame
 # Initialize the pygame
 pygame.init()
 
-# Create the screen , hieght = 800 and width = 600
+# Create the screen , width = 800 and height = 600
 screen = pygame.display.set_mode((800,600)) 
 
 # Title and Icon
@@ -13,14 +13,26 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("spaceship.png")   
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load("player.png")
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX,playerY))   # blit is for drawing
+
 # Game Loop
 # Prevents screen from closing until you quit
 running = True  # make false to close screen
 while running:
+
+    # RGB - Red, Green, Blue 
+    screen.fill((255, 0, 0))      # 0,0,0 = black
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # RGB - Red, Green, Blue 
-    screen.fill((0, 0, 0))      # 0,0,0 = black
+
+    player()    #must draw player after drawing screen so it appears on top
     pygame.display.update()     # update screen
